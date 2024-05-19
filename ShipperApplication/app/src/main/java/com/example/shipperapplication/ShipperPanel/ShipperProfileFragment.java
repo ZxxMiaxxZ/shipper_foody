ff19075c516cb9b6ace96e01205dd53d18d08786
Nguyen Duy Manh <duym63094@gmail.com>
2024-05-19 16:12:37 +0700

    oke

---
+++ ShipperApplication/app/src/main/java/com/example/shipperapplication/ShipperPanel/ShipperProfileFragment.java
@@ -0,0 +1,21 @@
+package com.example.shipperapplication.ShipperPanel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.shipperapplication.R;
+
+public class ShipperProfileFragment  extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_shipper_profile,null);
+        getActivity().setTitle("Profile");
+        return v;
+    }
+}
