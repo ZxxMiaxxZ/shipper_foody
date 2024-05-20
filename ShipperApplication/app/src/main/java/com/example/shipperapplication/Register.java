7166d02bd0013c4497bea474cdd9b3aa131c34e3
Nguyen Duy Manh <duym63094@gmail.com>
2024-05-20 14:12:45 +0700

    API login

--- ShipperApplication/app/src/main/java/com/example/shipperapplication/Register.java
+++ ShipperApplication/app/src/main/java/com/example/shipperapplication/Register.java
@@ -2,11 +2,25 @@ package com.example.shipperapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Register extends AppCompatActivity {
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private static final String BASE_URL = "http://10.0.2.2:3302/";
     private MaterialButton btnRegister, btnGoToLogin;
     private TextInputLayout inputName, inputEmail, inputPassword;
 
@@ -21,20 +35,56 @@ public class Register extends AppCompatActivity {
         btnRegister = findViewById(R.id.button_register);
         btnGoToLogin = findViewById(R.id.button_login);
 
-        // Set the click listener for the "Register" button
-        btnRegister.setOnClickListener(v -> {
-            // You can add your registration logic here
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-            // After registration logic, navigate to the OTP verification screen
-            Intent intent = new Intent(Register.this, VerifyOtpActivity.class);
-            startActivity(intent);
-        });
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
 
-        // Set the click listener for the "Go To Login" button
+        btnRegister.setOnClickListener(v -> handleRegister());
+
         btnGoToLogin.setOnClickListener(v -> {
             Intent intent = new Intent(Register.this, MainActivity.class);
             startActivity(intent);
         });
+    }
+
+    private void handleRegister() {
+        String username = inputName.getEditText().getText().toString().trim();
+        String email = inputEmail.getEditText().getText().toString().trim();
+        String password = inputPassword.getEditText().getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(Register.this, "Please fill in both fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        map.put("email",email);
+
+        Call<Driver> call = retrofitInterface.executeRegister(map);
+
+        call.enqueue(new Callback<Driver>() {
+            @Override
+            public void onResponse(Call<Driver> call, Response<Driver> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    Driver result = response.body();
+                    Intent intent = new Intent(Register.this, ShipperPanelBottomNavigationActivity.class);
+                    startActivity(intent);
+                    finish(); // Đóng màn hình hiện tại nếu muốn
+                } else {
+                    Toast.makeText(Register.this, "Register failed, please try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<Driver> call, Throwable t) {
+                Toast.makeText(Register.this, "Network error: " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
--- ShipperApplication/app/src/main/java/com/example/shipperapplication/VerifyOtpActivity.java
+++ ShipperApplication/app/src/main/java/com/example/shipperapplication/VerifyOtpActivity.java
@@ -1,14 +1,84 @@
-package com.example.shipperapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class VerifyOtpActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.verify_otp);
-    }
-}
+//package com.example.shipperapplication;
+//
+//import androidx.appcompat.app.AlertDialog;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.Toast;
+//
+//import com.google.android.material.button.MaterialButton;
+//import com.google.android.material.textfield.TextInputLayout;
+//
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//import retrofit2.Response;
+//import retrofit2.Retrofit;
+//import retrofit2.converter.gson.GsonConverterFactory;
+//
+//public class VerifyOtpActivity extends AppCompatActivity {
+//
+//    private Retrofit retrofit;
+//    private RetrofitInterface retrofitInterface;
+//    private static final String BASE_URL = "http://10.0.2.2:3302/";
+//    private MaterialButton btnVerify;
+//    private TextInputLayout txtVerify;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.verify_otp);
+//        btnVerify = findViewById(R.id.button_verify_otp);
+//        txtVerify = findViewById(R.id.edit_otp);
+//
+//        retrofit = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
+//        retrofitInterface = retrofit.create(RetrofitInterface.class);
+//        btnVerify.setOnClickListener(v -> handleVerify());
+//    }
+//
+//    private void handleVerify() {
+//        String otp = txtVerify.getEditText().getText().toString().trim();
+//
+//        // Check if OTP field is empty
+//        if (otp.isEmpty()) {
+//            txtVerify.setError("Please enter OTP");
+//            return;
+//        }
+//
+//        // Call your Retrofit API interface method to verify OTP
+//        Call<Void> call = retrofitInterface.verifyOtp(otp);
+//
+//        // Execute the network request asynchronously
+//        call.enqueue(new Callback<Void>() {
+//            @Override
+//            public void onResponse(Call<Void> call, Response<Void> response) {
+//                // Handle successful response
+//                if (response.isSuccessful()) {
+//                    Driver result = response.body();
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(VerifyOtpActivity.this);
+//                    builder.setTitle("Welcome ")
+//                            .setMessage("Successful);
+//                    startActivity(new Intent(VerifyOtpActivity.this, NextActivity.class));
+//                    finish(); // Finish this activity to prevent going back to OTP screen
+//                } else {
+//                    // OTP verification failed
+//                    // Show appropriate error message
+//                    txtVerify.setError("Incorrect OTP. Please try again.");
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Void> call, Throwable t) {
+//                // Handle network errors or request failure
+//                // Show error message or retry request
+//                Toast.makeText(VerifyOtpActivity.this, "Failed to verify OTP. Please try again later.", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+//
+//}
