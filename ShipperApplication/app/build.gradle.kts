7ec01aea41d6a0df74e3521a1a2369bb372ce596
Nguyen Duy Manh <duym63094@gmail.com>
2024-05-25 13:24:20 +0700

    Revert "update grade"
    This reverts commit c0834729751fa608d3cfd36d9722204271bc906f.

--- ShipperApplication/app/build.gradle.kts
+++ ShipperApplication/app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.android.gms:play-services-location:18.0.0")
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
     //implementation ("com.google.code.gson:gson:2.8.6")
 
